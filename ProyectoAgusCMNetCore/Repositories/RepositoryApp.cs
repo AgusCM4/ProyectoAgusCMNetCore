74e0b239b7258fbf6099497f74eade6865f9952a
AgusCM4 <agustincamposmartinez@hotmail.com>
2022-01-21 14:08:32 +0100

    commit

---
+++ ProyectoAgusCMNetCore/Repositories/RepositoryApp.cs
@@ -0,0 +1,18 @@
+﻿using ProyectoAgusCMNetCore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoAgusCMNetCore.Repositories
+{
+    public class RepositoryApp
+    {
+        private MainContext context;
+
+        public RepositoryApp(MainContext context)
+        {
+            this.context = context;
+        }
+    }
+}
--- ProyectoAgusCMNetCore/Startup.cs
+++ ProyectoAgusCMNetCore/Startup.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProyectoAgusCMNetCore.Data;
+using ProyectoAgusCMNetCore.Repositories;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -23,6 +26,11 @@ namespace ProyectoAgusCMNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string cadenaconexion = this.Configuration.GetConnectionString("cadenaAzure");
+
+            services.AddTransient<RepositoryApp>();
+            services.AddDbContext<MainContext>(options=>options.UseSqlServer(cadenaconexion));
+
             services.AddControllersWithViews();
         }
 
